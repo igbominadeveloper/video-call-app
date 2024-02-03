d613189408107cdb90799cf805b81a4323ed8ff3
Favour Afolayan <favourafolayan@gmail.com>
2024-02-03 12:43:28 +0100

    Complete the navbar

--- components/navbar.vue
+++ components/navbar.vue
@@ -52,7 +52,7 @@ const links: NavLink[] = [
 
 <template>
   <div
-    class="h-[5rem] border-b border-b-lightgray grid grid-cols-[306px_4fr_1fr]"
+    class="h-[5rem] border-b border-b-lightgray grid grid-cols-[306px_2fr_1fr]"
   >
     <header class="flex gap-3 items-center pl-14">
       <div class="text-orange border-4 size-10 rounded-full" />
@@ -60,7 +60,7 @@ const links: NavLink[] = [
     </header>
 
     <nav>
-      <ul class="flex gap-11 items-center h-full">
+      <ul class="flex gap-11 items-center h-full z-10">
         <li
           v-for="link in links"
           :key="link.title"
@@ -69,20 +69,44 @@ const links: NavLink[] = [
           <span
             class="h-[0.3rem] absolute top-0 w-0 bg-[transparent] group-hover:w-full group-hover:bg-lightblue rounded-sm transition-all duration-300"
           ></span>
-          <nuxt-link :title="link.title" :to="link.url">{{
-            link.title
-          }}</nuxt-link>
+          <nuxt-link
+            :title="link.title"
+            :to="link.url"
+            class="cursor-pointer hover:text-lightblue"
+            >{{ link.title }}</nuxt-link
+          >
 
-          <Icon
-            name="material-symbols:keyboard-arrow-down"
-            v-if="link.children?.length"
-          />
+          <template v-if="link.children?.length">
+            <Icon class="ml-1" name="material-symbols:keyboard-arrow-down" />
+
+            <div
+              class="border -bottom-[11.3rem] z-20 border-lightgray divide-y divide-lightgray rounded-lg shadow-md absolute flex flex-col min-w-max"
+            >
+              <nuxt-link
+                :title="childLink.title"
+                :to="childLink.url"
+                class="px-6 py-5 cursor-pointer hover:text-lightblue"
+                v-for="childLink in link.children"
+                >{{ childLink.title }}</nuxt-link
+              >
+            </div>
+          </template>
         </li>
       </ul>
     </nav>
 
-    <div>
-      <nuxt-link>Help</nuxt-link>
+    <div class="flex items-center w-full">
+      <nuxt-link class="font-bold mr-4 pr-4 py-2 border-r border-darkblue/70"
+        >Help</nuxt-link
+      >
+
+      <div class="flex items-center gap-3 pl-4">
+        <div class="size-12 rounded-lg bg-orange/50"></div>
+        <div class="flex justify-between flex-col gap-2">
+          <h1 class="font-bold">Cody Fisher</h1>
+          <p class="text-darkgray/70 text-sm">cody.fisher@gmail.com</p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
