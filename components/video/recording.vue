c3d3fec2ddb3709d02f11956772592d120e44a2e
Favour Afolayan <favourafolayan@gmail.com>
2024-02-05 21:58:55 +0100

    Show video duration

--- components/video/recording.vue
+++ components/video/recording.vue
@@ -18,10 +18,32 @@ const getSize = (sizeInBytes: number) => {
 
   return `${sizeInBytes.toFixed(2)} ${units[units.length - 1]}`;
 };
+
+function formatDuration(durationInSeconds: number) {
+  const hours = Math.floor(durationInSeconds / 3600);
+  const minutes = Math.floor((durationInSeconds % 3600) / 60);
+  const seconds = durationInSeconds % 60;
+
+  const formattedHours = String(hours).padStart(2, '0');
+  const formattedMinutes = String(minutes).padStart(2, '0');
+  const formattedSeconds = String(seconds).padStart(2, '0');
+
+  if (hours > 0) {
+    return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
+  } else {
+    return `${formattedMinutes}:${formattedSeconds}`;
+  }
+}
 </script>
 
 <template>
-  <NuxtImg :src="recording.thumbnail" class="w-32 h-16 rounded-md" />
+  <div class="relative w-32 h-16">
+    <NuxtImg :src="recording.thumbnail" class="size-full rounded-md relative" />
+    <span
+      class="text-[11px] bg-lighterblue p-1.5 py-0.5 rounded text-white absolute bottom-1 right-1"
+      >{{ formatDuration(recording.duration) }}</span
+    >
+  </div>
   <div class="flex flex-col justify-between gap-3">
     <p class="text-darkblue">{{ recording.title }}</p>
     <span class="text-lightestblue max-h-[35px] max-w-[224px]">{{
