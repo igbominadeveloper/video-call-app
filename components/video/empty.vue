0fdef6ff1b0d2ff710f4779ac249c794abc21abd
Favour Afolayan <favourafolayan@gmail.com>
2024-02-05 18:16:01 +0100

    Add the application footer section

--- components/video/empty.vue
+++ components/video/empty.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <section
+  <div
     class="mt-8 flex flex-col items-center justify-center border border-dashed border-lightestblue h-[563px] rounded-3xl"
   >
     <NuxtImg src="/media-file.png" class="w-[223px] h-[176px]" />
@@ -15,5 +15,18 @@
     </div>
 
     <video-buttons class="mt-9 flex-row-reverse" />
-  </section>
+  </div>
+
+  <div class="mt-11 flex gap-[79px] items-center mb-36">
+    <NuxtImg src="/screenshot.png" class="shadow-md" />
+
+    <div class="flex flex-col gap-9">
+      <p class="max-w-[378px] text-darkblue font-medium text-xl">
+        Want more controls & better quality recording?
+      </p>
+      <basebutton class="max-w-[278px]" type="primary" shape="largestRound">
+        Download the desktop recorder
+      </basebutton>
+    </div>
+  </div>
 </template>
