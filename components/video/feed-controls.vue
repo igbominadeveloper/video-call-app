e9e785aa93e7481b4624f1693215f722cbb7f895
Favour Afolayan <favourafolayan@gmail.com>
2024-02-12 15:44:08 +0100

    Fix style

--- components/video/feed-controls.vue
+++ components/video/feed-controls.vue
@@ -1,36 +1,23 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const buttons = [
+  { icon: 'mdi:microphone', color: 'text-white' },
+  { icon: 'heroicons:video-camera', color: 'text-white' },
+  { icon: 'ic:outline-screen-share', color: 'text-white' },
+  { icon: 'material-symbols:call-end-sharp', color: 'text-red' },
+];
+</script>
 
 <template>
   <div
-    class="py-3 w-full relative bg-darkblue flex justify-center gap-6 items-center"
+    class="py-3 w-full relative bg-darkblue flex rounded-b-lg justify-center gap-6 items-center"
   >
     <base-button
       size="none"
       class="p-3 bg-darkergray hover:bg-lightblue border-none"
       shape="fullRound"
+      v-for="button in buttons"
     >
-      <Icon name="mdi:microphone" class="size-6 text-white" />
-    </base-button>
-    <base-button
-      size="none"
-      class="p-3 bg-darkergray hover:bg-lightblue border-none"
-      shape="fullRound"
-    >
-      <Icon name="heroicons:video-camera" class="size-6 text-white" />
-    </base-button>
-    <base-button
-      size="none"
-      class="p-3 bg-darkergray hover:bg-lightblue border-none"
-      shape="fullRound"
-    >
-      <Icon name="ic:outline-screen-share" class="size-6 text-white" />
-    </base-button>
-    <base-button
-      size="none"
-      class="p-3 bg-darkergray hover:bg-lightblue border-none"
-      shape="fullRound"
-    >
-      <Icon name="material-symbols:call-end-sharp" class="size-6 text-red" />
+      <Icon :name="button.icon" class="size-6" :class="button.color" />
     </base-button>
   </div>
 </template>
